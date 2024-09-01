336ee7a3b2d4a4a99c787164b245247e8970105a
Shivam <alvindevil568@gmail.com>
2024-09-01 17:59:06 +0530

    -assets-folder-upt

--- Frontend/src/component/Navbar.jsx
+++ Frontend/src/component/Navbar.jsx
@@ -4,7 +4,7 @@ import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 import { Link, animateScroll as scroll } from 'react-scroll';
 
-import logo from '../../public/assets/icons/AgreeConnect.png';
+import logo from '../assets/AgreeConnect.png';
 import zIndex from '@mui/material/styles/zIndex';
 const Navbar = () => {
 
